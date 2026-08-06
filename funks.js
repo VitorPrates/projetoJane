a36af81ab55cb3912c0fc2853521770bb1c1a9bf
VitorPrates <vitoraaprates@gmail.com>
2026-08-05 21:59:40 -0300

    adicionando área de fotos dos clientes

--- funks.js
+++ funks.js
@@ -1,5 +1,9 @@
 const banner_imgs = document.querySelectorAll(".banner_img")
 let banner_atual = 0
+const clientes_imgs = document.querySelectorAll(".cliente_img")
+let clientes_atual = 0
+
+
 function mudar_banner()
 {
     banner_atual += 1
@@ -7,10 +11,20 @@ function mudar_banner()
     {
         banner_atual = 0
     }
-    console.log(banner_atual);
-    
     banner_imgs.forEach((banner) =>{
         banner.style.transform = `translateX(-${100*banner_atual}%)`
     })
 }
-setInterval(mudar_banner,3000)
+function mudar_cliente()
+{
+    clientes_atual += 1
+    if(clientes_atual >= clientes_imgs.length)
+    {
+        clientes_atual = 0
+    }    
+    clientes_imgs.forEach((cliente) =>{
+        cliente.style.transform = `translateX(-${100*banner_atual}%)`
+    })
+}
+setInterval(mudar_banner,5000)
+setInterval(mudar_cliente,2100)
