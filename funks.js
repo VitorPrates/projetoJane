153f1c3e37260c10fb0943b68fad1236807f0b59
VitorPrates <vitoraaprates@gmail.com>
2026-08-11 21:44:36 -0300

    Navegações de menu adicionadas

--- funks.js
+++ funks.js
@@ -8,6 +8,15 @@ let clientes_atual = 0
 let zap_icon = document.querySelector(".conversar_zap")
 let campo_conversa_zap = document.querySelector(".campo_conversas_zap")
 
+//navegação de displays principais
+const promocoes_btn = document.querySelector(".promocoes_btn")
+const sericos_btn = document.querySelector(".servicos_btn")
+const contato_btn = document.querySelector(".contato_btn")
+const selected_ection = document.querySelector(".selected-section")
+const display_main_promocoes = document.querySelector(".display_main_promocoes")
+const display_main_servicos = document.querySelector(".display_main_servicos")
+const display_main_contato = document.querySelector(".display_main_contato")
+
 
 function mudar_banner()
 {
@@ -34,6 +43,48 @@ function mudar_cliente()
 setInterval(mudar_banner,5000)
 setInterval(mudar_cliente,2100)
 
+//abrir contato via chat
 zap_icon.addEventListener("click", () =>{
     campo_conversa_zap.classList.toggle("mostrar_zap")
 })
+
+//funções para mudar display selecionado
+function mudar_main_display(display)
+{
+    switch (display) {
+        case "promo":
+            display_main_promocoes.style.transform = "translatex(0)"
+            display_main_servicos.style.transform = "translatex(110%)"
+            display_main_contato.style.transform = "translatex(220%)"
+            selected_ection.innerHTML = "Promoções"
+            break;
+        case "servi":
+            display_main_promocoes.style.transform = "translatex(-110%)"
+            display_main_servicos.style.transform = "translatex(0%)"
+            display_main_contato.style.transform = "translatex(110%)"
+            selected_ection.innerHTML = "Serviços"
+            break;
+        case "contact":
+            display_main_promocoes.style.transform = "translatex(-220%)"
+            display_main_servicos.style.transform = "translatex(-110%)"
+            display_main_contato.style.transform = "translatex(0%)"
+            selected_ection.innerHTML = "Contato"
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+
+promocoes_btn.addEventListener("click", ()=>{
+    mudar_main_display("promo")
+})
+sericos_btn.addEventListener("click", ()=>{
+    mudar_main_display("servi")
+})
+contato_btn.addEventListener("click", ()=>{
+    mudar_main_display("contact")
+})
+
